7b4bed17243981d56f065a3363553420a809cc90
haofrank <18546749+haofrank@users.noreply.github.com>
2024-04-29 14:12:31 -0400

    update test fuse adam

--- diff_gaussian_rasterization/test_fuse_adam.py
+++ diff_gaussian_rasterization/test_fuse_adam.py
@@ -1,4 +1,5 @@
 import torch
+import time
 import torch.nn as nn
 from diff_gaussian_rasterization import FusedAdam
 
@@ -10,7 +11,7 @@ torch.cuda.manual_seed_all(42)
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
-        self.fc1 = nn.Linear(100, 20)
+        self.fc1 = nn.Linear(10000, 20)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(20, 1)
 
@@ -21,23 +22,29 @@ class SimpleNet(nn.Module):
         return x
 
 model = SimpleNet().to("cuda:0")
-print("################ Test Fused Adam #################")
-optimizer = FusedAdam(model.parameters(), lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, weight_decay=0.0)
-# print("################ Test Pytorch Adam #################")
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0)
+# print("################ Test Fused Adam #################")
+# optimizer = FusedAdam(model.parameters(), lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, weight_decay=0.0)
+print("################ Test Pytorch Adam #################")
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0)
 
 
 criterion = nn.MSELoss()
 
 torch.set_printoptions(precision=10)
-x = torch.randn(100, 100).to("cuda:0")
+x = torch.randn(100, 10000).to("cuda:0")
 y = torch.randn(100, 1).to("cuda:0")
 
+total_time = 0
+epochs = 20
 for epoch in range(20):
     optimizer.zero_grad()
     outputs = model(x)
     loss = criterion(outputs, y)
     loss.backward()
+    t1 = time.time()
     optimizer.step()
+    t2 = time.time()
+    total_time += (t2 - t1)
 
     print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+print(f"Adam optimizer AVG time: {total_time/epochs}")
